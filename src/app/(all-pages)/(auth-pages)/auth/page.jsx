81db219a6583adf95f93646234562151a9077fe1
Buddika Kasun <buddikakasun80@gmail.com>
2024-07-08 21:32:48 +0530

    final select auth page

--- src/app/(all-pages)/(auth-pages)/auth/page.jsx
+++ src/app/(all-pages)/(auth-pages)/auth/page.jsx
@@ -3,7 +3,6 @@
 import Link from "next/link";
 import style from "./auth.module.css";
 import { useSearchParams } from "next/navigation";
-import Footer from "@/components/footer/Footer";
 
 const AuthPage = () => {
     const mode = useSearchParams().get("mode"); // get router query (path from "mode")
@@ -12,26 +11,34 @@ const AuthPage = () => {
     return (
         <div className={style.container}>
 
-            <div className={`${style.overlay} ${isLogin? '' : style.overlayRight}`}>
+            <div className={`${style.overlayLeft} ${isLogin? '' : style.overlayRight}`}>
                 <img src="/logo.png" alt="" />
             </div>
             
-            <div className={`${style.login} ${isLogin? style.activeLogin : ''}`}>
-                <div className={style.h}>Log In</div>
-                <form action="#" className={style.form}>
-                    aaaa
-                    <Link href={"/auth?mode=register"}>Create</Link>
-                </form>
-                <div className={style.footer}>All Right Reserved</div>
+            <div className={`${style.boxRight} ${isLogin? '' : style.boxLeft}`}>
+                {/* white box */}
             </div>
 
-            <div className={`${style.create} ${isLogin? '' : style.activeCreate}`}>
-                <div className={style.h}>Create an Account</div>
-                <form action="#" className={style.form}>
-                    aaaa
-                    <Link href={"/auth?mode=login"}>Login</Link>
-                </form>
-                <div className={style.footer}>All Right Reserved</div>
+            <div className={style.containerBottom}>
+
+                <div className={`${style.childContainer} ${isLogin? style.activeCreateContainer : style.deactiveCreateContainer}`}>    
+                    <div className={style.h}>Create an Account</div>
+                    <form action="#" className={style.form}>
+                        aaaa
+                        <Link href={"/auth?mode=login"}>Login</Link>
+                    </form>
+                    <div className={style.footer}>All Right Reserved</div>
+                </div>
+
+                <div className={`${style.childContainer} ${isLogin? style.deactiveLoginContainer : style.activeLoginContainer}`}>
+                    <div className={style.h}>Log In</div>
+                    <form action="#" className={style.form}>
+                        aaaa
+                        <Link href={"/auth?mode=register"}>Create</Link>
+                    </form>
+                    <div className={style.footer}>All Right Reserved</div>
+                </div>
+
             </div>
 
         </div>
