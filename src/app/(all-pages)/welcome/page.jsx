071bf1c7e749b898ce362b6bcf135d4bc338d009
Buddika Kasun <buddikakasun80@gmail.com>
2024-07-08 09:23:54 +0530

    .

--- src/app/(all-pages)/welcome/page.jsx
+++ src/app/(all-pages)/welcome/page.jsx
@@ -7,24 +7,24 @@ import Footer from "@/components/footer/Footer";
 const WelcomePage = () => {
     return (
         <>
-        <div className={style.container}>
-            <div className={style.imageContainer}>
-                <img src="/logo.png" alt="" />
-                <div className={style.textContainer}>
-                    <div className={style.h1}>
-                        <span>Welcome</span>&nbsp;
-                        <span className={style.child2}>to</span>&nbsp;
-                        <span>UniCore</span>
+            <div className={style.container}>
+                <div className={style.imageContainer}>
+                    <img src="/logo.png" alt="" />
+                    <div className={style.textContainer}>
+                        <div className={style.h1}>
+                            <span>Welcome</span>&nbsp;
+                            <span className={style.child2}>to</span>&nbsp;
+                            <span>UniCore</span>
+                        </div>
+                        <div className={style.h2}>University Management System</div>
                     </div>
-                    <div className={style.h2}>University Management System</div>
+                </div>
+                <div className={style.buttonField}>
+                    <Link href={'/login'} className={style.button}>Log in</Link>
+                    <Link href={'/register'} className={style.button}>Register</Link>
                 </div>
             </div>
-            <div className={style.buttonField}>
-                <Link href={'/login'} className={style.button}>Log in</Link>
-                <Link href={'/register'} className={style.button}>Register</Link>
-            </div>
-        </div>
-        <Footer />
+            <Footer />
         </>
     )
 }
