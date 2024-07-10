bf9f4b7bb691ce1b8b297e9e20bfe606afc115f9
Buddika Kasun <buddikakasun80@gmail.com>
2024-07-10 13:49:21 +0530

    loading page component

---
+++ src/app/loadingPage/page.jsx
@@ -0,0 +1,38 @@
+"use client"
+
+import Spinner from "@/components/spinner/Spinner";
+import style from "./loadingPage.module.css";
+import { useEffect, useState } from "react";
+
+const Loading = () => {
+
+    const [spinnerSize, setSpinnerSize] = useState(100); // Default size
+
+    useEffect(() => {
+
+        const handleResize = () => {
+            // Example logic for determining size based on viewport dimensions
+            const width = window.innerWidth;
+            const height = window.innerHeight;
+            
+            // Calculate spinner size as a percentage of viewport width or height
+            const size = Math.min(width, height) * 0.2; // 20% of the smaller dimension (width or height)
+            
+            // Set size
+            setSpinnerSize(size);
+        };
+  
+        handleResize();
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+
+    }, []);
+
+    return (
+        <div className={style.container}>
+           <Spinner type={"hash"} color={"rgb(18, 102, 211)"} size={spinnerSize}/> 
+        </div>
+    );
+}
+
+export default Loading;
