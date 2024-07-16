b58974ed9f8b15ce447ed8c5415cdd59e54192f5
Buddika Kasun <buddikakasun80@gmail.com>
2024-07-16 15:44:04 +0530

    new welcome page

---
+++ src/app/(all-pages)/welcomePage2/page.jsx
@@ -0,0 +1,36 @@
+"use client"
+
+import Link from "next/link";
+import style from "./welcomePage1.module.css";
+import Footer from "@/components/footer/Footer";
+
+const WelcomePage = () => {
+    return (
+        <>
+            <div className={style.container}>
+
+                <div className={style.imageContainer}>
+                    <img src="/logo.png" alt="" />
+                </div>
+
+                <div className={style.textContainer}>
+                    <div className={style.h1}>
+                        <span>Welcome</span>&nbsp;
+                        <span className={style.child2}>to</span>&nbsp;
+                        <span>UniCore</span>
+                    </div>
+                    <div className={style.h2}>University Management System</div>
+                </div>
+
+                <div className={style.buttonField}>
+                    <Link href={"/auth?mode=login"} className={style.button}>Log in</Link>
+                    <Link href={"/auth?mode=register"} className={style.button}>Register</Link>
+                </div>
+
+            </div>
+            <Footer />
+        </>
+    )
+}
+
+export default WelcomePage;
