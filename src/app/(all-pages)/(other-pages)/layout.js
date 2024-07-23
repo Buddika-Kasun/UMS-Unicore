2ef60bbe2d30794853e004d572e4587aaebe81b9
Buddika Kasun <buddikakasun80@gmail.com>
2024-07-23 12:25:47 +0530

    Common layout changes

--- src/app/(all-pages)/(other-pages)/layout.js
+++ src/app/(all-pages)/(other-pages)/layout.js
@@ -1,21 +1,42 @@
 "use client"
+
 import Footer from "@/components/footer/Footer";
 import Header from "@/components/header/Header";
 import style from "./layout.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Nav from "@/components/Nav/Nav";
-import Nav2 from "@/components/Nav1/Nav2";
+import { usePathname } from "next/navigation";
+import LoadingComp from "../loadingPage/page";
 
 const CommonLayout = ({children}) => {
+
+    const currentPath = usePathname();
+
     const [hamClick, setHamClick] = useState(false);
+    const [clickedPath, setClickedPath] = useState('/');
+    const [loading, setLoading] = useState(false);
+
+    const handleClickedPath = (value) => {
+        setClickedPath(value);
+    }
+
+    useEffect(() => {
+        setLoading(currentPath !== clickedPath);
+    },[currentPath, clickedPath]);
+
+    console.log("Current path = ", currentPath);
+    console.log("Clicked path = ", clickedPath);
+    console.log(loading);
+
     return (
         <div>
             <Header hamClick={hamClick} setHamClick={setHamClick}/>
             <div className= {style.innerContainer}>
-                {/* <Nav hamClick={hamClick}/> */}
-                <Nav2 hamClick={hamClick} />
+                <Nav hamClick={hamClick} clickedPath={handleClickedPath}/>
                 <div className={style.child}>
-                    {children}
+                    {loading ? <div className={style.loadingComp}><LoadingComp /></div> : children}
+                    {/* {loading && <LoadingComp />} */}
+                    {/* {children} */}
                 </div>
             </div>
             {/* <Footer /> */}
