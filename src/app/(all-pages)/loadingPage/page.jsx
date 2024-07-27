7c7050db6035ba7aaf13652f1e4674572c76e383
Buddika Kasun <buddikakasun80@gmail.com>
2024-07-27 14:33:51 +0530

    Add color variable in loading page

--- src/app/(all-pages)/loadingPage/page.jsx
+++ src/app/(all-pages)/loadingPage/page.jsx
@@ -4,7 +4,7 @@ import Spinner from "@/components/spinner/Spinner";
 import style from "./loadingPage.module.css";
 import { useEffect, useState } from "react";
 
-const LoadingComp = () => {
+const LoadingComp = ({color}) => {
 
     const [spinnerSize, setSpinnerSize] = useState(100); // Default size
 
@@ -14,14 +14,14 @@ const LoadingComp = () => {
             // Example logic for determining size based on viewport dimensions
             const width = window.innerWidth;
             const height = window.innerHeight;
-            
+
             // Calculate spinner size as a percentage of viewport width or height
             const size = Math.min(width, height) * 0.17; // 17% of the smaller dimension (width or height)
-            
+
             // Set size
             setSpinnerSize(size);
         };
-  
+
         handleResize();
         window.addEventListener("resize", handleResize);
         return () => window.removeEventListener("resize", handleResize);
@@ -30,7 +30,7 @@ const LoadingComp = () => {
 
     return (
         <div className={style.container}>
-           <Spinner type={"hash"} color={"rgb(18, 102, 211)"} size={spinnerSize}/> 
+           <Spinner type={"hash"} color={color || "rgb(18, 102, 211)"} size={spinnerSize}/>
         </div>
     );
 }
