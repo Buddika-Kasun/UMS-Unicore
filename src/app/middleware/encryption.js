9685369e3f76059ab61e4070c2747b15bba038ac
Buddika Kasun <buddikakasun80@gmail.com>
2024-07-30 17:03:43 +0530

    Encryption

---
+++ src/app/middleware/encryption.js
@@ -0,0 +1,35 @@
+const CryptoJS = require('crypto-js');
+
+// Encrypt function
+export function encrypt(value, secretKey) {
+    try {
+        const ciphertext = CryptoJS.AES.encrypt(value, secretKey).toString();
+        return ciphertext;
+    } catch (error) {
+        console.error('Error encrypting value:', error);
+        return null;
+    }
+}
+
+// Decrypt function
+export function decrypt(encryptedvalue, secretKey) {
+    try {
+        // Check if encryptedvalue is defined
+        if (!encryptedvalue) {
+            throw new Error('Encrypted value is undefined');
+        }
+
+        const bytes = CryptoJS.AES.decrypt(encryptedvalue, secretKey);
+        const originalvalue = bytes.toString(CryptoJS.enc.Utf8);
+
+        // Ensure the decryption resulted in a valid string
+        if (!originalvalue) {
+            throw new Error('Failed to decrypt value');
+        }
+
+        return originalvalue;
+    } catch (error) {
+        console.error('Error decrypting value:', error);
+        return null;
+    }
+}
