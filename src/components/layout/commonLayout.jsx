faf1f2651970e2ce3c953900304ac840be552617
Buddika Kasun <buddikakasun80@gmail.com>
2024-08-02 16:21:35 +0530

    Common layout component

---
+++ src/components/layout/commonLayout.jsx
@@ -0,0 +1,44 @@
+"use client"
+
+import Footer from "@/components/footer/Footer";
+import Header from "@/components/header/Header";
+import style from "./layout.module.css";
+import { useEffect, useState } from "react";
+import Nav from "@/components/nav/Nav";
+import { usePathname } from "next/navigation";
+import LoadingComp from "@/app/(all-pages)/loadingPage/page";
+
+const CommonLayout = ({children,sessionData}) => {
+    console.log(sessionData);
+    const currentPath = usePathname();
+
+    const [hamClick, setHamClick] = useState(false);
+    const [clickedPath, setClickedPath] = useState(currentPath);
+    const [loading, setLoading] = useState(false);
+
+    const handleClickedPath = (value) => {
+        setClickedPath(value);
+    }
+
+    useEffect(() => {
+        setLoading(currentPath !== clickedPath);
+    },[currentPath, clickedPath]);
+
+    return (
+        <div className={style.container}>
+            <div className={style.header}><Header hamClick={hamClick} setHamClick={setHamClick} sessionData={sessionData}/></div>
+            <div className= {style.innerContainer}>
+                <Nav hamClick={hamClick} clickedPath={handleClickedPath}/>
+                <div className={style.child}>
+                    {children}
+                    <div className={`${style.loadingComp} ${loading && style.loadingCompHide}`}>
+                        <LoadingComp color={"rgba(0, 0, 0, 0.8)"}/>
+                    </div>
+                </div>
+            </div>
+            {/* <Footer /> */}
+        </div>
+    )
+}
+
+export default CommonLayout;
