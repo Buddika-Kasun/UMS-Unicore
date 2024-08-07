9c7874527243bf834144294b21931d6b51b5dffe
Buddika Kasun <buddikakasun80@gmail.com>
2024-08-07 20:06:47 +0530

    Auth con fig

---
+++ src/app/api/auth/auth.js
@@ -0,0 +1,47 @@
+import { findUserByEmail } from "@/queries/users";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import {authConfig} from "./auth.config";
+import bcrypt from "bcryptjs";
+
+export const {
+    handlers: {GET, POST},
+    auth,
+    signIn,
+    signOut
+} = NextAuth({
+    ...authConfig,
+    // Configure one or more authentication providers
+    providers: [
+        CredentialsProvider({
+            async authorize(credentials) {
+                if (credentials === null) return null;
+                console.log("credencials = ", credentials);//
+                try {
+                    const user = findUserByEmail(credentials?.email);
+                    console.log('user =',user);//
+                    if (user) {
+                        const isMatch = user?.pw === credentials?.pw;
+                        //const isMatch = await bcrypt.compare(credentials?.pw, user?.pw); // DB
+                        console.log(user);//
+                        if (isMatch) {
+                            return user;
+                        }
+                        else {
+                            throw ("406 - Check your password!");
+                        }
+                    }
+                    else{
+                        throw ("401 - User not found");
+                    }
+                }
+                catch(err) {
+                    console.log('In auth.js = ', err);//
+                    throw (err);
+                }
+            }
+        }),
+        // ...add more providers here
+    ],
+    secret: process.env.NEXTAUTH_SECRET, // Ensure this is set
+});
