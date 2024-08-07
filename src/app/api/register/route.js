cfcf09d7243f589f65f2f836af5672f565900ca4
Buddika Kasun <buddikakasun80@gmail.com>
2024-08-07 20:08:25 +0530

    Register backend

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,21 +1,39 @@
-"use server"
+import "server-only";
 
 import { dbConnect } from "@/lib/mongo";
-import { createUser } from "@/queries/users";
+import { createUser, findUserByEmail } from "@/queries/users";
 import { NextResponse } from "next/server";
-import { decrypt } from "@/app/middleware/encryption";
-import { hash } from "@/app/middleware/hashing";
+import { decrypt } from "@/security/encryption";
+import { hash } from "@/security/hashing";
+import { xssSanitize } from "@/security/purify";
 
 export async function POST(req) {
 
-    const {name, faculty, type, email, encryptedPw} = await req.json();
+    let {name, faculty, type, email, pw} = await req.json();
 
-    const decryptedPw = await decrypt(encryptedPw, 'bikz');
+    // XSS Protection
+    name = xssSanitize(name);
+    faculty = xssSanitize(faculty);
+    type = xssSanitize(type);
+    email = xssSanitize(email);
+    pw = xssSanitize(pw);
 
-    const hashedPw = await hash(decryptedPw, 8);
-/*
+    // Validate the email using a regular expression pattern
+    if (!/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/.test(email)) {
+        return NextResponse.json({ message: 'Invalid email format.' }, { status: 409 });
+    }
+
+    const existingUser = findUserByEmail(email);
+    if (existingUser) {
+        return NextResponse.json({ message: 'User already exists with this email' }, { status: 409 });
+    }
+
+    const decryptedPw = await decrypt(pw);
+
+    const hashedPw = await hash(decryptedPw);
+
     const newUser = {name, faculty, type, email, pw: hashedPw};
-
+/*
     await dbConnect();
 
     try {
@@ -27,5 +45,5 @@ export async function POST(req) {
 
     return NextResponse.json({message: "User has been created"}, { status: 201 });
 */
-    return NextResponse.json({encryptedPw, decryptedPw, hashedPw}, { status: 201 });
+    return NextResponse.json({newUser}, { status: 201 });
 }
