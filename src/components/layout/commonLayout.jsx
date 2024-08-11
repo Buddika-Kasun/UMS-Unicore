ae05841b3897c86df8dee35d08e2c44b4beea4b6
Buddika Kasun <buddikakasun80@gmail.com>
2024-08-11 19:17:41 +0530

    Session time out message

--- src/components/layout/commonLayout.jsx
+++ src/components/layout/commonLayout.jsx
@@ -7,14 +7,31 @@ import { useEffect, useState } from "react";
 import Nav from "@/components/nav/Nav";
 import { usePathname } from "next/navigation";
 import LoadingComp from "@/app/(all-pages)/loadingPage/page";
+import Link from "next/link";
 
-const CommonLayout = ({children,sessionData}) => {
-    console.log(sessionData);
+const CommonLayout = ({children, sessionData, sessionExpiry}) => {
+    console.log(sessionData,sessionExpiry);
     const currentPath = usePathname();
 
     const [hamClick, setHamClick] = useState(false);
     const [clickedPath, setClickedPath] = useState(currentPath);
     const [loading, setLoading] = useState(false);
+    const [showTimeoutMessage, setShowTimeoutMessage] = useState(false);
+
+    useEffect(() => {
+        if (sessionData) {
+
+          // Set a timeout to show the session timeout message
+          const timeout = setTimeout(() => {
+            setShowTimeoutMessage(true);
+          }, sessionExpiry);;
+
+          // Cleanup timeout on component unmount
+          return () => {
+            clearTimeout(timeout);
+          };
+        }
+      }, [sessionData]);
 
     const handleClickedPath = (value) => {
         setClickedPath(value);
@@ -26,7 +43,18 @@ const CommonLayout = ({children,sessionData}) => {
 
     return (
         <div className={style.container}>
+
+            {showTimeoutMessage &&
+                <div className={style.sessionExpiryContainer}>
+                    <div className={style.card}>
+                        <div className={style.message}>Your session has expired.<br/>Please log in again.</div>
+                        <Link className={style.btn} href={'/authPages?mode=login'}>Ok</Link>
+                    </div>
+                </div>
+            }
+
             <div className={style.header}><Header hamClick={hamClick} setHamClick={setHamClick} sessionData={sessionData} clickedPath={handleClickedPath}/></div>
+
             <div className= {style.innerContainer}>
                 <Nav hamClick={hamClick} clickedPath={handleClickedPath}/>
                 <div className={style.child}>
