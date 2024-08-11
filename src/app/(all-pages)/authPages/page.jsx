e3c9d89727ac881efcb1287f682178b8ce6d936c
Buddika Kasun <buddikakasun80@gmail.com>
2024-08-11 17:31:59 +0530

    Modified

--- src/app/(all-pages)/authPages/page.jsx
+++ src/app/(all-pages)/authPages/page.jsx
@@ -5,13 +5,30 @@ import style from "./authPages.module.css";
 import { useSearchParams } from "next/navigation";
 import RegisterForm from "@/components/authForms/registerForm";
 import LoginForm from "@/components/authForms/loginForm";
+import LoadingComp from "../loadingPage/page";
+import { useState } from "react";
 
 const AuthPage = () => {
     const path = useSearchParams().get("mode"); // get router query (path from "mode")
     const mode = path !== "register";
 
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    if(error !== null) {
+        console.log(error);
+        alert(error);
+        setError(null);
+    };
+
     return (
         <div className={style.container}>
+
+            {isLoading &&
+                <div className={`${style.loadingContainer} ${mode? style.loadingRight : style.loadingLeft}`}>
+                    <LoadingComp size={0.15}/>
+                </div>
+            }
 
             <div className={`${style.overlayLeft} ${mode? '' : style.overlayRight}`}>
                 <img src="/logo.png" alt="" />
@@ -23,9 +40,9 @@ const AuthPage = () => {
 
             <div className={style.containerBottom}>
 
-                <RegisterForm isRegister={mode} />
+                <RegisterForm isRegister={mode} setIsLoading={setIsLoading} setError={setError}/>
 
-                <LoginForm isLogin={mode} />
+                <LoginForm isLogin={mode} setIsLoading={setIsLoading} setError={setError}/>
 
             </div>
 
