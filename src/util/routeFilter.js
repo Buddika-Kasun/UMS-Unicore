bd81fd05e661abc54e808e81106b1e9244deb507
Buddika Kasun <buddikakasun80@gmail.com>
2024-08-13 16:32:17 +0530

    Accessible route filter by user

---
+++ src/util/routeFilter.js
@@ -0,0 +1,26 @@
+import filterMenuByRole from "./menuFilter";
+import menuData from '@/data/menuData';
+
+const filterRouteByRole = (userRole) => {
+
+    const filteredMenu = filterMenuByRole(menuData, userRole);
+
+    const route = [];
+
+    filteredMenu.forEach(section => {
+        section.menu.forEach(menu => {
+            if(menu.haveSubmenu) {
+                menu.submenu.forEach(submenu => {
+                    route.push(submenu.path);
+                })
+            }
+            else {
+                route.push(menu.path);
+            }
+        })
+    })
+
+    return route;
+}
+
+export default filterRouteByRole;
