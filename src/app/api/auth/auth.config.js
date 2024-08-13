6fef29eb2bbc94b4c76746df0448648559e88300
Buddika Kasun <buddikakasun80@gmail.com>
2024-08-13 16:42:22 +0530

    Auth config update

--- src/app/api/auth/auth.config.js
+++ src/app/api/auth/auth.config.js
@@ -3,4 +3,29 @@ export const authConfig = {
         strategy: 'jwt',
     },
     providers: [],
+    callbacks: {
+        async session({ session, token, user }) {
+            // Customize the session object here
+            if (token) {
+                session.user.id = token.id;
+                session.user.role = token.role; // Example of adding a custom property
+            }
+            return session;
+        },
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.id;
+                token.role = user.role; // Example of adding a custom property
+            }
+            return token;
+        }
+    },
+    secret: process.env.NEXTAUTH_SECRET, // Ensure this is set
+    session: {
+        // Configure session expiry
+        maxAge: 1 * 60 * 60, // Session will expire in 1 hours (in seconds)
+        //maxAge: 30,
+        //updateAge: 30 * 60, // Session will be updated every 30 min (in seconds)
+        updateAge: 1 * 60,
+    },
 }
