d7ec37e596e76f477900281317d40e8a9f8d5cc4
Buddika Kasun <buddikakasun80@gmail.com>
2024-08-13 16:41:11 +0530

    Middleware update

--- src/middleware.js
+++ src/middleware.js
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import { authConfig } from "./app/api/auth/auth.config";
 import NextAuth from "next-auth";
 import { PUBLIC_ROUTES,LOGIN,ROOT } from "./lib/routes";
+import filterRouteByRole from "./util/routeFilter";
 
 const {auth} = NextAuth(authConfig);
 
@@ -10,7 +11,11 @@ export async function middleware(req) {
     const {nextUrl} = req;
 
     const session = await auth();
-    //console.log(session);
+
+    const userRole = session?.user?.role;
+    //const userRole = 'User';
+    //console.log(userRole);
+    const ROLE_BASED_ROUTES = filterRouteByRole(userRole);
 
     //console.log("midleware");
 
@@ -19,10 +24,16 @@ export async function middleware(req) {
 
     const isPublicRoute = (PUBLIC_ROUTES.find(route => nextUrl.pathname.includes(route))
     || nextUrl.pathname === ROOT);
-    //console.log(isPublicRoute);
+
+    const isRoleBasedRoute = ROLE_BASED_ROUTES.find(route => nextUrl.pathname.includes(route));
+    //console.log(isRoleBasedRoute);
 
     if(!isAuthenticated && !isPublicRoute) {
         return NextResponse.redirect(new URL(LOGIN, nextUrl));
+    }
+
+    if(isAuthenticated && !isRoleBasedRoute){
+        return NextResponse.redirect(new URL('/dashboard', nextUrl));
     }
 
 }
