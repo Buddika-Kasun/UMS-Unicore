71eb017cf2ee8c3eeaeb774c6792f3f9484cf975
Buddika Kasun <buddikakasun80@gmail.com>
2024-08-15 12:40:49 +0530

    Message handle

--- src/components/layout/commonLayout.jsx
+++ src/components/layout/commonLayout.jsx
@@ -3,20 +3,31 @@
 import Footer from "@/components/footer/Footer";
 import Header from "@/components/header/Header";
 import style from "./layout.module.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Nav from "@/components/nav/Nav";
-import { usePathname } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 import LoadingComp from "@/app/(all-pages)/loadingPage/page";
 import Link from "next/link";
+import { toast } from "react-toastify";
+import { BiSolidLock, BiSolidLockOpen } from "react-icons/bi";
+import handleMiddlewareMsg from "@/util/handleMiddlewareMsg";
 
 const CommonLayout = ({children, sessionData, sessionExpiry}) => {
     //console.log(sessionData,sessionExpiry);
+
     const currentPath = usePathname();
 
     const [hamClick, setHamClick] = useState(false);
     const [clickedPath, setClickedPath] = useState(currentPath);
     const [loading, setLoading] = useState(false);
     const [showTimeoutMessage, setShowTimeoutMessage] = useState(false);
+    const [autoHide, setAutoHide] = useState(true);
+
+    const message = useSearchParams().get("message");
+    const messageDisplayedRef = useRef(false);
+
+    //toast.dismiss();
+    message === null && toast.dismiss();
 
     useEffect(() => {
         if (sessionData) {
@@ -24,7 +35,7 @@ const CommonLayout = ({children, sessionData, sessionExpiry}) => {
           // Set a timeout to show the session timeout message
           const timeout = setTimeout(() => {
             setShowTimeoutMessage(true);
-          }, sessionExpiry);;
+          }, sessionExpiry);
 
           // Cleanup timeout on component unmount
           return () => {
@@ -39,7 +50,30 @@ const CommonLayout = ({children, sessionData, sessionExpiry}) => {
 
     useEffect(() => {
         setLoading(currentPath !== clickedPath);
+        autoHide && !(currentPath !== clickedPath) && setHamClick(false);
     },[currentPath, clickedPath]);
+
+    // Handle outside click of nav bar
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (hamClick && autoHide && (event.target.closest(`.${style.child}`) || event.target.closest(`.${style.header}`))) {
+                setHamClick(false);
+            }
+        };
+
+        document.addEventListener('click', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, [hamClick,autoHide]);
+
+    useEffect(() => {
+        if (!messageDisplayedRef.current && message) {
+            handleMiddlewareMsg(message);
+            messageDisplayedRef.current = true; // Mark the message as displayed
+        }
+    }, [message]);
 
     return (
         <div className={style.container}>
@@ -53,7 +87,21 @@ const CommonLayout = ({children, sessionData, sessionExpiry}) => {
                 </div>
             }
 
-            <div className={style.header}><Header hamClick={hamClick} setHamClick={setHamClick} sessionData={sessionData} clickedPath={handleClickedPath}/></div>
+            <div className={style.header}>
+                <Header hamClick={hamClick} setHamClick={setHamClick} sessionData={sessionData} clickedPath={handleClickedPath}/>
+            </div>
+
+            <div
+                className={`${style.lock} ${hamClick && style.lockShow}`}
+                onClick={()=>setAutoHide(pre => !pre)}
+            >
+                {autoHide?
+                    <BiSolidLockOpen size={12} />
+                :
+                    <BiSolidLock size={12} />
+                }
+                <div className={style.hint}>Auto hide {autoHide? 'enable': 'disable'}</div>
+            </div>
 
             <div className= {style.innerContainer}>
                 <Nav hamClick={hamClick} clickedPath={handleClickedPath} role={sessionData.role}/>
