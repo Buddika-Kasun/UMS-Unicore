e0b6b7ba348cf0effd37e0cfda130cd95131e022
Buddika Kasun <buddikakasun80@gmail.com>
2024-08-16 20:09:42 +0530

    Add DB

--- src/lib/mongo.js
+++ src/lib/mongo.js
@@ -2,7 +2,7 @@ import { connect } from "mongoose";
 
 export async function dbConnect() {
     try {
-        const conn = await connect(String(process.env.MONGO_DB_CONNECTION_STRING));
+        const conn = await connect(process.env.MONGO_DB_CONNECTION_STRING);
         return conn;
     }
     catch(err) {
