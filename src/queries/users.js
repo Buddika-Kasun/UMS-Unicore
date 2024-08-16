6d8b45eeb50635a15296d3045fc6a14486e46dde
Buddika Kasun <buddikakasun80@gmail.com>
2024-08-16 20:10:08 +0530

    Update with DB

--- src/queries/users.js
+++ src/queries/users.js
@@ -1,6 +1,6 @@
 import "server-only";
 
-//import { User } from "@/model/user-model";    // DB
+import { User } from "@/model/user-model";    // DB
 
 export async function createUser(user) {
     try {
@@ -48,14 +48,14 @@ const us = [
     }
 ];                                     // DB
 
-export /*async*/ function findUserByEmail(email) {
+export async function findUserByEmail(email) {
     try {
-        //const user = await User.findOne({ email: email }).exec(); // Use .exec() for better error handling
-        //return user; // Will return null if no user is found
-        const user = us.find(user => user.email === email);
-        return user || null;
+        const user = await User.findOne({ email: email }); // Use .exec() for better error handling
+        return user; // Will return null if no user is found
+        //const user = us.find(user => user.email === email);
+        //return user || null;
     } catch (err) {
         console.error("Error finding user by email:", err);
-        throw new Error(err);
+        //throw new Error(err);
     }
 }
