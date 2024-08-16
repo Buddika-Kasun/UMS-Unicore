b70a7903b74348df62fc978a02ec8c8d03874de4
Buddika Kasun <buddikakasun80@gmail.com>
2024-08-16 20:24:11 +0530

    Update with DB

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -9,34 +9,35 @@ import { xssSanitize } from "@/security/purify";
 
 export async function POST(req) {
 
-    let {name, faculty, type, email, pw} = await req.json();
-
-    // XSS Protection
-    name = xssSanitize(name);
-    faculty = xssSanitize(faculty);
-    type = xssSanitize(type);
-    email = xssSanitize(email);
-    pw = xssSanitize(pw);
-
-    // Validate the email using a regular expression pattern
-    if (!/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/.test(email)) {
-        return NextResponse.json({ message: 'Invalid email format.' }, { status: 409 });
-    }
-
-    const existingUser = findUserByEmail(email);
-    if (existingUser) {
-        return NextResponse.json({ message: 'User already exists with this email' }, { status: 409 });
-    }
-
-    const decryptedPw = await decrypt(pw);
-
-    const hashedPw = await hash(decryptedPw);
-
-    const newUser = {name, faculty, type, email, pw: hashedPw};
-/*
-    await dbConnect();
-
     try {
+
+        let {name, faculty, type, email, pw} = await req.json();
+
+        // XSS Protection
+        name = xssSanitize(name);
+        faculty = xssSanitize(faculty);
+        type = xssSanitize(type);
+        email = xssSanitize(email);
+        pw = xssSanitize(pw);
+
+        // Validate the email using a regular expression pattern
+        if (!/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/.test(email)) {
+            return NextResponse.json({ message: 'Invalid email format.' }, { status: 409 });
+        }
+
+        await dbConnect();
+
+        const existingUser = await findUserByEmail(email);
+        if (existingUser) {
+            return NextResponse.json({ message: 'User already exists with this email' }, { status: 409 });
+        }
+
+        const decryptedPw = await decrypt(pw);
+
+        const hashedPw = await hash(decryptedPw);
+
+        const newUser = {name, faculty, type, email, pw: hashedPw};
+
         await createUser(newUser);
     }
     catch(err) {
@@ -44,6 +45,9 @@ export async function POST(req) {
     }
 
     return NextResponse.json({message: "User has been created"}, { status: 201 });
-*/
-    return NextResponse.json({newUser}, { status: 201 });
+//*/
+    //return NextResponse.json({newUser}, { status: 201 });
+    //return NextResponse.json({message: "User has been created"}, { status: 201 });
+    //return NextResponse.json({message: 'error'}, { status: 500 });
+    //return NextResponse.json({ message: 'User already exists with this email' }, { status: 409 });
 }
