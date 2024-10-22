ec3ae05148c6fec026c4b5c66659047d787d37b3
Buddika Kasun <buddikakasun80@gmail.com>
2024-10-22 16:29:38 +0530

    Update gester master API

--- src/app/api/pages/gestor/master/route.js
+++ src/app/api/pages/gestor/master/route.js
@@ -28,9 +28,12 @@ const sanitize = (value) => {
     return sanitizedData;
 }
 
+
+
+//  CREATE  //
 export async function POST(req) {
 
-    //console.log("api/pages/gestor/master = Catch req");
+    //console.log("api/pages/gestor/master = Catch insert req");
 
     try {
 
@@ -51,9 +54,44 @@ export async function POST(req) {
     return NextResponse.json({message: "Saved Location"}, { status: 200 });
 }
 
+
+
+//  RAED    //
+export async function GET(req) {
+
+    try {
+
+        const url = new URL(req.url);
+        const fetchLast = url.searchParams.get('last');
+
+        await dbConnect();
+
+        if (fetchLast) {
+            const preDocID = await Location.findOne({}, { docID: 1, _id: 0 }).sort({ _id: -1 });
+            const id = parseInt(preDocID.docID.split('/')[2]) + 1;
+            const newdocId = `${preDocID.docID.split('/')[0]}/LOC/${id}`;
+
+            return NextResponse.json(newdocId, { status: 200 });
+        }
+        else{
+            const locations = await Location.find({},{_id: 0, __v:0}).lean();
+
+            return NextResponse.json(locations, { status: 200 });
+        }
+
+    }
+    catch(err) {
+        console.log("error = ",err)
+        return NextResponse.json({message: err.message}, { status: 500 });
+    }
+
+}
+
+
+//  UPDATE  //
 export async function PUT(req) {
 
-    //console.log("api/pages/gestor/master = Catch req");
+    //console.log("api/pages/gestor/master = Catch update req");
 
     try {
 
@@ -76,4 +114,32 @@ export async function PUT(req) {
     }
 
     return NextResponse.json({message: "Updated Location"}, { status: 200 });
+}
+
+
+
+//  DELETE   //
+export async function DELETE(req) {
+
+    console.log("api/pages/gestor/master = Catch delete req");
+
+    try {
+        const data = await req.json();
+
+        await dbConnect();
+
+        const result = await Location.deleteOne({docID: data.docId});
+
+        if (result.deletedCount === 0) {
+            return NextResponse.json({message: "No document found with the specified Doc ID."}, { status: 500 });
+        }
+
+    }
+    catch(err) {
+        console.log(err)
+        return NextResponse.json({message: err.message}, { status: 500 });
+    }
+
+    return NextResponse.json({message: "Deleted Location"}, { status: 200 });
+
 }
