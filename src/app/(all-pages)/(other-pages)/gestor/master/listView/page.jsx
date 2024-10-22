845a807747d19688a38c1d7719276b55fb244ad3
Buddika Kasun <buddikakasun80@gmail.com>
2024-10-22 12:01:15 +0530

    Gestor master list view page

---
+++ src/app/(all-pages)/(other-pages)/gestor/master/listView/page.jsx
@@ -0,0 +1,34 @@
+"use server"
+
+import React from "react";
+import ListView from "@/components/listView/ListView";
+import { dbConnect } from "@/lib/mongo";
+import { Location } from "@/model/location-model";
+
+const listView = async() => {
+
+  await dbConnect();
+
+  const formData = await Location.find({},{_id: 0, __v:0}).lean();
+
+  const headers = [
+    "Doc ID",
+    "Doc Date",
+    "Faculty",
+    "Cost Center",
+    "Location Type",
+    "Active",
+    "Building No",
+    "Floor No",
+    "Location Name",
+    "Location Code",
+  ];
+
+  return (
+    <div>
+      <ListView data={formData} headers={headers} path='/gestor/master' />
+    </div>
+  );
+};
+
+export default listView;
