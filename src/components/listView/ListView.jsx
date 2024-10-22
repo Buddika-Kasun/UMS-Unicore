54fcb1e8c4c4aa12fe1c2e37ba11d689ff8d518e
Buddika Kasun <buddikakasun80@gmail.com>
2024-10-22 16:29:59 +0530

    Update list view component

--- src/components/listView/ListView.jsx
+++ src/components/listView/ListView.jsx
@@ -5,11 +5,15 @@ import styles from './listView.module.css';
 import { useRouter } from "next/navigation";
 import { HiArrowLeft } from "react-icons/hi";
 import SubLoading from "../loading/SubLoading";
+import { MdEdit, MdDeleteForever } from "react-icons/md";
+import axios from "axios";
+import { toast } from "react-toastify";
 
-const ListView = ({ headers, data, path}) => {
+const ListView = ({ headers, initData, updatePath, reqPath}) => {
 
     const router = useRouter();
 
+    const [data, setData] = useState(initData);
     const [isloading, setIsLoading] = useState(false);
 
     // Check if there's data to display
@@ -17,9 +21,45 @@ const ListView = ({ headers, data, path}) => {
         return <div className={styles.container}>No data available</div>;
     };
 
+    const fetchLocations = async () => {
+        try {
+            const res = await axios.get(`${reqPath}`);
+            setData(res.data);
+        } catch (error) {
+            toast.error('An unexpected error occurred while fetching locations.');
+        }
+    };
+
     const handleEdit = (docId) => {
         setIsLoading(true);
-        router.push(`${path}${docId}`);
+        router.push(`${updatePath}${docId}`);
+    }
+
+    const handleDelete = async(docId) => {
+        setIsLoading(true);
+
+        try {
+
+            const res = await axios.delete( `${reqPath}`, {data: {docId}});
+
+            if (res.status === 200) {
+
+                fetchLocations();
+
+                setIsLoading(false);
+
+                toast.success(res.data.message, {
+                    autoClose: 2000,
+                });
+            }
+            else {
+                throw err;
+            }
+        }
+        catch(err) {
+            setIsLoading(false);
+            toast.error('An unexpected error occurred while processing.');
+        }
     }
 
     const goBack = () => {
@@ -49,7 +89,8 @@ const ListView = ({ headers, data, path}) => {
                     <td key={idx}>{value}</td>
                     ))}
                     <td className={styles.edit}>
-                        <button className={styles.button} onClick={() => handleEdit(row.docID)}>Edit</button>
+                        <button title="Edit" className={styles.editBtn} onClick={() => handleEdit(row.docID)}><MdEdit /></button>
+                        <button title="Delete" className={styles.deleteBtn} onClick={() => handleDelete(row.docID)}><MdDeleteForever /></button>
                     </td>
                 </tr>
                 ))}
