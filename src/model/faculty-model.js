a25e863433d282b85a6b62fdb5a5304f90d0c612
Sasindu Rashmika <133813189+SasinduRash@users.noreply.github.com>
2024-10-23 23:34:42 +0530

    faculty model modify

--- src/model/faculty-model.js
+++ src/model/faculty-model.js
@@ -8,15 +8,20 @@ const facultySchema = new Schema({
     facultyCode: {
         required: true,
         type: String,
+        unique: true,
     },
     facultyName: {
         required: true,
         type: String,
+        unique: true,
     },
     Active: {
         required: true,
         type: String,
     },
 });
+
+facultySchema.index({ facultyCode: 1 }, { unique: true });
+facultySchema.index({ facultyName: 1 }, { unique: true });
 
 export const Faculty = models.Faculty || model("Faculty", facultySchema);
