9d747f39cddd00a2ad75f49a4de48665499cad95
Buddika Kasun <buddikakasun80@gmail.com>
2024-10-24 02:54:15 +0530

    Verification form update

--- src/app/(all-pages)/(other-pages)/setup/pendingUsers/page.jsx
+++ src/app/(all-pages)/(other-pages)/setup/pendingUsers/page.jsx
@@ -9,11 +9,14 @@ const PendingUserCreation = async() => {
   const facultys = await Faculty.find({}, { facultyName: 1, _id: 0 }).lean();
   const facultyNames = facultys.map(faculty => faculty.facultyName);
 
-  const pendingUsers = await User.find({ 'verification.state': 'request' });
-  console.log(pendingUsers);
+  const pendingUsers = await User.find(
+    {'verification.state': 'request' },
+    { 'verification.createDate': 1, faculty: 1, name: 1, type: 1, _id: 1 }
+  );
+  //console.log(pendingUsers);
 
   return (
-      <PendingUsersComp facultys={facultyNames}/>
+      <PendingUsersComp facultys={facultyNames} users={pendingUsers} />
     );
 };
 
